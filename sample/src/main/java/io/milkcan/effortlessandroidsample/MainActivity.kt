f9be227394a51831106fea5c8f7b4569f6265d48
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-06-05 12:15:35 -0500

    Updated build to support Bintray uploading

---
+++ sample/src/main/java/io/milkcan/effortlessandroidsample/MainActivity.kt
@@ -0,0 +1,23 @@
+package io.milkcan.effortlessandroidsample
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import io.milkcan.effortlessandroid.toastLong
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        toastLong("Toasty!")
+    }
+
+}
