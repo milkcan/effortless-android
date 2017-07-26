b5019480b5be9fd3b92f61ddbac6bf3d4afdc3f7
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-07-26 14:33:00 -0500

    Added extension which returns a scaled & center cropped Bitmap

--- effortless-android-base/src/main/java/io/milkcan/effortlessandroid/BitmapExtensions.kt
+++ effortless-android-base/src/main/java/io/milkcan/effortlessandroid/BitmapExtensions.kt
@@ -3,7 +3,9 @@
 package io.milkcan.effortlessandroid
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Matrix
+import android.graphics.RectF
 
 /**
  * A collection of helpful functions that extend [Bitmap].
@@ -14,9 +16,11 @@ import android.graphics.Matrix
  */
 
 /**
- * @param maxWidth
- * @param maxHeight
- * @return
+ * Scales a Bitmap while retaining it's original aspect ratio.
+ *
+ * @param maxWidth The max width of the scaled bitmap
+ * @param maxHeight The max height of the scaled bitmap
+ * @return The scaled bitmap
  */
 fun Bitmap.scaleAndMaintainAspectRatio(maxWidth: Int, maxHeight: Int): Bitmap {
     if (maxHeight > 0 && maxWidth > 0) {
@@ -47,4 +51,34 @@ fun Bitmap.mirror(): Bitmap {
     matrix.preScale(-1f, 1f)
 
     return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, false)
+}
+
+/**
+ * Scales and center crops a Bitmap to exactly [newWidth] x [newHeight].
+ *
+ * @param source
+ * @param newWidth The width of the finished Bitmap
+ * @param newHeight The height of the finished Bitmap
+ * @return The scaled and cropped bitmap
+ */
+fun Bitmap.scaleCenterCrop(newWidth: Int, newHeight: Int): Bitmap {
+    val sourceWidth = this.width
+    val sourceHeight = this.height
+
+    val scale = Math.max(newWidth.toFloat() / sourceWidth, newHeight.toFloat() / sourceHeight)
+
+    val scaledWidth = scale * sourceWidth
+    val scaledHeight = scale * sourceHeight
+
+    val left = (newWidth - scaledWidth) / 2
+    val top = (newHeight - scaledHeight) / 2
+
+    val targetRect = RectF(left, top, left + scaledWidth, top + scaledHeight)
+
+    val scaledBitmap = Bitmap.createBitmap(newWidth, newHeight, this.config)
+
+    val canvas = Canvas(scaledBitmap)
+    canvas.drawBitmap(this, null, targetRect, null)
+
+    return scaledBitmap
 }
