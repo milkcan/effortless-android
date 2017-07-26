3626fbfbe5c3b75888abc4f9af3ee170ef9add33
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-07-26 14:32:24 -0500

    Removed ternary infix, see: https://discuss.kotlinlang.org/t/ternary-operator/2116/17

--- effortless-android-stdlib/src/main/java/io/milkcan/effortlessandroid/stdlib/BooleanExtensions.kt
+++ effortless-android-stdlib/src/main/java/io/milkcan/effortlessandroid/stdlib/BooleanExtensions.kt
@@ -15,10 +15,3 @@ package io.milkcan.effortlessandroid.stdlib
  * @return 1 if the [Boolean] value is true, 0 if false.
  */
 inline fun Boolean.asInt(): Int = if (this) 1 else 0
-
-/**
- * Provides a "Kotlinized" ternary operator similar to Java.
- *
- * Original source: https://stackoverflow.com/questions/16336500/kotlin-ternary-conditional-operator/39687177#39687177
- */
-inline infix fun <T> Boolean.then(t: T): T? = if (this) t else null
