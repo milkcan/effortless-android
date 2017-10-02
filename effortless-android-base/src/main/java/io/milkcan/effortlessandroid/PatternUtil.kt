e8847d66a9c7f3db8739297d83aa51e9f42089a5
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-10-02 15:10:36 -0500

    Adding extensions for IP/phone/email matching (using android.util.Patterns)

---
+++ effortless-android-base/src/main/java/io/milkcan/effortlessandroid/PatternUtil.kt
@@ -0,0 +1,34 @@
+@file:JvmName("PatternUtil")
+@file:Suppress("NOTHING_TO_INLINE", "unused")
+
+package io.milkcan.effortlessandroid
+
+/**
+ * @author Eric Bachhuber (bachhuberdesign@gmail.com)
+ * @version 1.0.3
+ * @since 1.0.3
+ */
+
+inline fun String?.isValidEmail(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
+
+inline fun CharSequence?.isValidEmail(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
+}
+
+inline fun String?.isValidIpAddress(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.IP_ADDRESS.matcher(this).matches()
+}
+
+inline fun CharSequence?.isValidIpAddress(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.IP_ADDRESS.matcher(this).matches()
+}
+
+inline fun String?.isValidPhone(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.PHONE.matcher(this).matches()
+}
+
+inline fun CharSequence?.isValidPhone(): Boolean {
+    return !this.isNullOrBlank() && android.util.Patterns.PHONE.matcher(this).matches()
+}
