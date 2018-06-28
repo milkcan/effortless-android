9ad9df24cc2dd78841b69acef3b54f90d168d0f8
Eric Bachhuber <bachhuberdesign@gmail.com>
2018-06-28 08:42:28 -0500

    Renamed Boolean.asInt() -> Boolean.toInt()

--- effortless-android-stdlib/src/main/java/io/milkcan/effortlessandroid/stdlib/BooleanExtensions.kt
+++ effortless-android-stdlib/src/main/java/io/milkcan/effortlessandroid/stdlib/BooleanExtensions.kt
@@ -7,11 +7,11 @@ package io.milkcan.effortlessandroid.stdlib
  * A collection of helpful functions that extend [Boolean].
  *
  * @author Eric Bachhuber (bachhuberdesign@gmail.com)
- * @version 1.0.2
+ * @version 1.1.0
  * @since 1.0.2
  */
 
 /**
  * @return 1 if the [Boolean] value is true, 0 if false.
  */
-inline fun Boolean.asInt(): Int = if (this) 1 else 0
+inline fun Boolean.toInt(): Int = if (this) 1 else 0
