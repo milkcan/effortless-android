84f54e24598c0d69bd9e358779ce6be6b600a356
Eric Bachhuber <bachhuberdesign@gmail.com>
2018-06-28 08:41:43 -0500

    Added DelayedOnClickListener with extension

--- effortless-android-base/src/main/java/io/milkcan/effortlessandroid/ViewExtensions.kt
+++ effortless-android-base/src/main/java/io/milkcan/effortlessandroid/ViewExtensions.kt
@@ -4,6 +4,7 @@
 package io.milkcan.effortlessandroid
 
 import android.view.View
+import io.milkcan.effortlessandroid.adapter.DelayedOnClickListener
 
 /**
  * A collection of helpful functions that extend [View].
@@ -38,3 +39,29 @@ fun View.gone() {
  * Sets a view to gone if currently visible, or visible if it is currently gone or invisible.
  */
 fun View.toggleVisibility(): Unit = if (visibility == View.VISIBLE) gone() else visible()
+
+/**
+ * Sets a click listener that ignores subsequent clicks within a certain threshold. By default, any
+ * clicks within 500 milliseconds of the initial click are ignored. Use when you want to prevent
+ * accidental double clicks on a View.
+ *
+ * @param function The function to be executed when [View] is clicked.
+ * @see View.setDelayedOnClickListener(function, int)
+ */
+inline fun View.setDelayedOnClickListener(crossinline function: (view: View) -> Unit) {
+    this.setOnClickListener(object : DelayedOnClickListener(500) {
+        override fun onSingleClick(view: View) = function(view)
+    })
+}
+
+/**
+ * Sets a click listener that ignores subsequent clicks within a certain threshold.
+ *
+ * @param function The function to be executed when [View] is clicked.
+ * @param delayMilliseconds Duration before the click listener is active again.
+ */
+inline fun View.setDelayedOnClickListener(crossinline function: () -> Unit, delayMilliseconds: Int) {
+    this.setOnClickListener(object : DelayedOnClickListener(delayMilliseconds) {
+        override fun onSingleClick(view: View) = function()
+    })
+}
